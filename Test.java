169937722f106ccfa9d4e121f7ff29a35493913e
嘤嘤嘤 <70877566+ohmyGod-cell@users.noreply.github.com>
2020-09-15 18:34:30 +0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class 	MaopaoTest {
+
+	@Test
+	public void test() {
+		int[] arr={11,6,7,8,3};
+		Example01.mpTest(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+}
+
